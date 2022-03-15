b3747b2dd4d1e238678d618a8f00dd7f01d86462
Lujain <82683327+LujainAlajmi@users.noreply.github.com>
2022-03-15 13:44:36 +0300

    Added customize qr code based on url

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import QrCode from "./image-qr-code.png";
+import QRCode from "qrcode.react";
 function App() {
+  const [qrText, setQrText] = useState("");
+  const [text, setText] = useState("");
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    setQrText(text);
+
+    return false;
+  }
   return (
     <div className="flex flex-col bg-LightGray w-screen h-screen justify-center">
       <div className="  w-80 bg-White m-auto rounded-[20px] items-center pb-4 drop-shadow-lg">
-        <img src={QrCode} alt="qr-code" className="w-72 my-4 mx-4 rounded-md" />
-        <h1 className="font-bold text-DarkBlue text-center px-14 text-lg ">
+        {/* <img src={QrCode} alt="qr-code" className="w-72 my-4 mx-4 rounded-md" /> */}
+        <div className="w-72 my-4 mx-4 rounded-md bg-BrightBlue py-16">
+          <QRCode
+            className="w-72 m-auto"
+            value={qrText}
+            size={160}
+            fgColor="#ffffff"
+            bgColor="#306efe"
+          />
+        </div>
+        {/* <h1 className="font-bold text-DarkBlue text-center px-14 text-lg ">
           Improve your front-end skills by building projects
         </h1>
         <p className=" text-GrayishBlue text-center py-3 text-sm px-10">
           Scan the QR code to visit Frontend Mentor and take your coding skills
           to the next level
-        </p>
+        </p> */}
+        <form onSubmit={handleSubmit}>
+          <div class="m-6">
+            <label class="block mb-2 text-sm font-medium text-DarkBlue">
+              Enter the URL
+            </label>
+            <input
+              type="text"
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+              placeholder="Enter URL"
+            />
+            <button
+              type="submit"
+              class=" px-5 py-2.5 bg-blue-700 hover:bg-blue-800 mt-3 text-white focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm w-full text-center"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
       </div>
-      <div className=" text-center text-xs text-blue mb-2">
+      <div className=" text-center text-xs text-Blue mb-2">
         Challenge by{" "}
         <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
           Frontend Mentor
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,16 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    colors: {
-      White: "#ffffff",
-      LightGray: "#d6e2f0",
-      GrayishBlue: "#7b879d",
-      DarkBlue: "hsl(218, 44%, 22%)",
-      blue: "hsl(228, 45%, 44%)",
+    extend: {
+      colors: {
+        White: "#ffffff",
+        LightGray: "#d6e2f0",
+        GrayishBlue: "#7b879d",
+        DarkBlue: "hsl(218, 44%, 22%)",
+        Blue: "hsl(228, 45%, 44%)",
+        BrightBlue: "#306efe",
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
